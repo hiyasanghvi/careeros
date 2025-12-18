d3ad245eaec93d2f3c7d619948a447e92fd1c3b8
Hiya Sanghvi <169714000+hiyasanghvi@users.noreply.github.com>
2025-12-18 12:48:51 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="CareerOS API")
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","https://carrer-os.onrender.com"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
